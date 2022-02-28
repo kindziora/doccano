4a0e291269a7ddc3357145600c97a477ca7cf42d
kindziora <kindziora@live.de>
2022-02-28 20:55:40 +0100

    created user registration via api and providing auth token via api

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
 
 from .serializers import UserSerializer
 from projects.permissions import IsProjectAdmin
@@ -17,10 +21,30 @@ class Me(APIView):
         return Response(serializer.data)
 
 
-class Users(generics.ListAPIView):
+class ListUsers(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated & IsProjectAdmin]
     pagination_class = None
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     search_fields = ("username",)
+
+
+class RegisterUser(generics.CreateAPIView):
+  serializer_class = UserSerializer
+  queryset = User.objects.all()
+  permission_classes = [
+        permissions.AllowAny
+  ]
+
+  def post(self, request, format='json'):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        if user:
+            token = Token.objects.create(user=user)
+            json = serializer.data
+            json['token'] = token.key
+            return Response(json, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
