e66d8f8ae1f384b8a21d48fdb4cd81dac3f21c5c
kindziora <kindziora@live.de>
2022-02-28 20:54:10 +0100

    created user registration via api and providing auth token via api

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,17 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "is_superuser", "is_staff")
+        fields = ("id", "username", 'password',)
+
+        
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 
-from .views import Me, Users
+from .views import Me, ListUsers, RegisterUser
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path(route="me", view=Me.as_view(), name="me"),
-    path(route="users", view=Users.as_view(), name="user_list"),
+    path(route="users", view=ListUsers.as_view(), name="user_list"),
+    path(route="register", view=RegisterUser.as_view(), name="user_register"),
+    path(route="auth-token",  view=ObtainAuthToken.as_view(), name='user_token'),
     path("auth/", include("dj_rest_auth.urls")),
 ]
