68eac2e7d3a9cf720997074fba36ed2c151571b4
kindziora <kindziora@live.de>
2022-03-11 13:31:06 +0100

    user creating

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,13 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            first_name = validated_data['first_name'],
-            last_name = validated_data['last_name'],
-            email = validated_data['email']
-        )
+       
+        user = get_user_model().objects.create_user(**validated_data)
         return user
     class Meta:
         model = get_user_model()
